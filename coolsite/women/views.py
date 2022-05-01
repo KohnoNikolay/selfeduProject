0c4938fdea564bfaf6bff415eda4505cf7f6e25b
Николай Кохно <90795569+KohnoNikolay@users.noreply.github.com>
2022-05-01 16:53:38 +0300

    #5. CRUD - основы ORM по работе с моделями | Django уроки #6. Шаблоны (templates). Начало | Django уроки

--- coolsite/women/views.py
+++ coolsite/women/views.py
@@ -1,15 +1,22 @@
 from django.http import HttpResponse, HttpResponseNotFound
 from django.shortcuts import render, redirect
+from .models import *
+
+menu = ["О сайте", "Добавить статью", "Обратная связь", "Войти", ]
 
 
-def index(request):  #HttpResponse
-    return HttpResponse("Страница приложения women")
+def index(request):
+    posts = Women.objects.all()
+    return render(request, 'women/index.html', {'posts': posts, 'menu': menu, 'title': 'Главная страница'})
+
+
+def about(request):
+    return render(request, 'women/about.html', {'menu': menu, 'title': 'О сайте'})
 
 
 def categories(request, catid):  #HttpResponse
     if (request.GET):
         print(request.GET)
-
     return HttpResponse(f"<h1>Статьи по категориям</h1><p>{catid}</p>")
 
 
